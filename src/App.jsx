4478413a16bc7dbc636fb2127096bdcf5fc164da
Maeve Collins <c.maeve@wustl.edu>
2025-04-01 16:18:05 -0500

    have square receive value prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
 
-  function handleClick() {
-    setValue('X');
-  }
-
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
@@ -28,24 +27,3 @@ function Square() {
   );
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-    <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </>
-  );
-}
